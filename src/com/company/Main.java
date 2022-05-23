20c9113fe7286b977c52a6488221cca80994caea
Leandro Berne <lcberne@gmail.com>
2022-05-22 21:23:26 -0300

    Ejercicio 3 de ITERATIVAS

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,39 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 7; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int j = 1; j <= 7; j++) {
+            for(int i = 1; i <= 7 - j; i++) {
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= j; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int j = 7; j >= 1; j--) {
+            for(int i = 1; i <= 8 - j; i++) {
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= j - 1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
