e361e1271eec77014cf184e424fd614241000785
Leandro Berne <lcberne@gmail.com>
2022-05-22 21:16:41 -0300

    Ejercicio 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa el nombre del cursante");
+        String nombre = sc.nextLine();
+        System.out.println("Ingresa la primera nota de " + nombre);
+        int nota1 = sc.nextInt();
+        System.out.println("Ingresa la segunda nota de " + nombre);
+        int nota2 = sc.nextInt();
+        System.out.println("Ingresa la tercera nota de " + nombre);
+        int nota3 = sc.nextInt();
+        int promedio = (nota1 + nota2 + nota3) / 3;
+        String estado;
+        if (promedio < 6) {
+            estado = "desaprobado";
+        } else if (promedio < 9) {
+            estado = "aprobado";
+        } else {
+            estado = "promocionado";
+        }
+        System.out.println("El/La cursante " + nombre + " tiene un promedio de " + promedio + " y su estado es " + estado);
     }
 }
