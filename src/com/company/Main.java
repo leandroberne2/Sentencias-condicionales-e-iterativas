b96f59c9bba284245be632c6410a4655b837aa73
Leandro Berne <lcberne@gmail.com>
2022-05-22 21:05:55 -0300

    Ejercicio 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Ingresa el primer número:");
+        int num1 = myScanner.nextInt();
+        System.out.println("Ingresa el segundo número:");
+        int num2 = myScanner.nextInt();
+        if (num1 > num2) {
+            System.out.println("El primer número: " + num1 + " es el mayor");
+        } else if (num2 > num1) {
+            System.out.println("El segundo número: " + num2 + " es el mayor");
+        } else {
+            System.out.println("Ambos números son iguales");
+        }
     }
 }
