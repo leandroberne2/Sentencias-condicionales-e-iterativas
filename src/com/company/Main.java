c4a1ed74ecb408702aa9070cd5339ed0ec8d9f73
Leandro Berne <lcberne@gmail.com>
2022-06-03 14:48:17 -0300

    Ejercicio 2 de Ejercitacion EXTRA

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa un valor y te diré los números primos entre el 1 y el valor ingresado");
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            if (esPrimo(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean esPrimo(int numero) {
+        if (numero == 0 || numero == 1 || numero == 4) {
+            return false;
+        }
+        for (int x = 2; x < numero / 2; x++) {
+            if (numero % x == 0)
+                return false;
+        }
+        return true;
     }
 }
