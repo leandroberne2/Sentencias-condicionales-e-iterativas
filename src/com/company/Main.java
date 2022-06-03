1c980552e9dab3f4dd4b80ab45ed1df8aef6dd76
Leandro Berne <lcberne@gmail.com>
2022-06-03 14:43:44 -0300

    Ejercicio 1 de Ejercitacion EXTRA

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa un número y te diré si es Primo o no");
+        int num = sc.nextInt();
+        if (esPrimo(num)) {
+            System.out.println("El número " + num + " SI es primo");
+        } else {
+            System.out.println("El número " + num + " NO es primo");
+        }
+    }
+
+    public static boolean esPrimo(int numero) {
+        if (numero == 0 || numero == 1 || numero == 4) {
+            return false;
+        }
+        for (int x = 2; x < numero / 2; x++) {
+            if (numero % x == 0)
+                return false;
+        }
+        return true;
     }
 }
